89fc6bc8cf008ae4571222e354583f2b7213dcf5
zool <pdackow@wp.pl>
2023-05-14 23:03:38 +0200

    Added assertion check

--- src/test/java/pl/testeroprogramowania/pages/LoggedUserPage.java
+++ src/test/java/pl/testeroprogramowania/pages/LoggedUserPage.java
@@ -1,10 +1,26 @@
 package pl.testeroprogramowania.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoggedUserPage {
 
+    @FindBy(xpath = "//h1[@class='entry-title']")
+    private WebElement entryTitle;
+
+    private WebDriver driver;
+
     public LoggedUserPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        this.driver = driver;
     }
+
+    public WebElement getEntryTitle() {
+        return entryTitle;
+    }
+
+
 
 }
--- src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
+++ src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
@@ -1,13 +1,19 @@
 package pl.testeroprogramowania.tests;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegisterTest extends BaseTest {
 
     @Test
     public void registerUserTest() {
-        new HomePage(driver).openMyAccountPage()
-                .registerUser("test@test.pl", "test@test.pl");
+        WebElement entryTitle = new HomePage(driver).openMyAccountPage()
+                .registerUser("test612@test.pl", "test612@test.pl").getEntryTitle();
+
+        Assert.assertTrue(entryTitle.isDisplayed());
+        Assert.assertEquals(entryTitle.getText(), "My account");
+
 
     }
 
